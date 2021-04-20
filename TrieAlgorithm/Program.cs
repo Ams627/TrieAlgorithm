048b7475eab9cc693a0f5f0a5dc9b6c3bb131704
Adrian Sims <adriansims62781@gmail.com>
2021-04-20 20:35:48 +0200

    Add project files.

---
+++ TrieAlgorithm/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace TrieAlgorithm
+{
+
+    class PathDir
+    {
+        public string Directory { get; set; }
+        public string Sha1Sum { get; set; }
+        public string Prefix { get; set; }
+    }
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var sysPaths = Environment.GetEnvironmentVariable("PATH")
+                    .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => new PathDir { Directory = x, Sha1Sum = Sha1SumString(x) });
+
+                var group = sysPaths.ToLookup(x => x.Directory);
+                foreach (var dup in group.Where(x=>x.Count() > 1))
+                {
+                    Console.WriteLine($"Directory {dup.Key} occurs twice in system Path");
+                }
+
+
+                var t1 = new ShaTrie();
+                var prefixes = t1.GetShortestPrefix(sysPaths.Select(x=>x.Sha1Sum));
+                sysPaths = sysPaths.Zip(prefixes, (a, b) => new PathDir { Directory = a.Directory, Sha1Sum = a.Sha1Sum, Prefix = b });
+
+                foreach (var p in sysPaths)
+                {
+                    Console.WriteLine($"{p.Sha1Sum} {p.Prefix} {p.Directory}");
+                }
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+
+        }
+
+        private static string Sha1SumString(string str)
+        {
+            var data = Encoding.ASCII.GetBytes(str);
+            var hashData = new SHA1Managed().ComputeHash(data);
+            return string.Concat(hashData.Select(x => $"{x:x2}"));
+        }
+    }
+}
---
+++ TrieAlgorithm/ShaTrie.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrieAlgorithm
+{
+    /// <summary>
+    /// It's a Trie, but just for Sha1s - so only for strings containing only hex digits
+    /// </summary>
+    public class ShaTrie
+    {
+        class TrieNode
+        {
+            public int Frequency { get; set; }
+            public TrieNode[] Next { get; } = new TrieNode[26];
+        }
+
+        private readonly TrieNode root = new TrieNode();
+        public void Insert(string str)
+        {
+            _ = str ?? throw new ArgumentException("parameter is null", nameof(str));
+            var currentNode = root;
+            foreach (var c in str)
+            {
+                var b = GetHex(c);
+                if (c < 0)
+                {
+                    throw new ArgumentException($"invalid sha1 character in '{str}'- all characters must be valid hex digits");
+                }
+                if (currentNode.Next[b] == null)
+                {
+                    currentNode.Next[b] = new TrieNode();
+                }
+                currentNode.Next[b].Frequency++;
+                currentNode = currentNode.Next[b];
+            }
+        }
+
+        private int GetHex(char c)
+        {
+            if (c < '0' || c > 'f') return -1;
+            if (c > '9' && c < 'A') return -1;
+            if (c > 'F' && c < 'a') return -1;
+            if (char.IsDigit(c)) return c - '0';
+            return char.ToUpper(c) - 'A' + 10;
+        }
+
+        public string[] GetShortestPrefix(IEnumerable<string> strings)
+        {
+            foreach (var s in strings)
+            {
+                Insert(s);
+            }
+
+            var list = new List<string>();
+
+            foreach (var s in strings)
+            {
+                var sb = new StringBuilder();
+                var currentNode = root;
+                foreach (var c in s)
+                {
+                    sb.Append(c);
+                    var b = GetHex(c);
+                    if (c < 0)
+                    {
+                        throw new ArgumentException($"invalid sha1 character in '{s}'- all characters must be valid hex digits");
+                    }
+
+                    if (currentNode.Next[b].Frequency == 1)
+                    {
+                        break;
+                    }
+                    currentNode = currentNode.Next[b];
+                }
+                list.Add(sb.ToString());
+            }
+
+            return list.ToArray();
+        }
+    }
+}
